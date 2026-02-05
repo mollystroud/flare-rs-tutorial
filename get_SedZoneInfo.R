34c47de4f8a7dce89ccc6f057194cb0b64f97b50
mollystroud <mollykstroud@gmail.com>
2026-02-05 11:54:22 -0500

    Updated to add # sed zone estimation based on depth

--- get_SedZoneInfo.R
+++ get_SedZoneInfo.R
@@ -21,14 +21,19 @@ get_sed_zone_data <- function(era5, depth){
   airtemp_amp <- (max(avg_airtemps$Temp) - min(avg_airtemps$Temp)) / 2
   airtemp_peakdoy <- which.max(avg_airtemps$Temp)
   if(depth <=5){
+    nzones <- 1
+    z1 <- depth + 1
     sed_amp <- airtemp_amp
     sed_doy <- airtemp_peakdoy
-    return(cbind(sed_amp, sed_doy))
-  } else {
-    z1 <- depth + 1 ### CHECK THIS? HOW ARE THE ZONE HEIGHTS CALCULATED
-    z2 <- z1/2
-    sed_amp_z1 <- airtemp_amp * exp(-z1/depth)
+    return(cbind(sed_amp, sed_doy, nzones, z1))
+  } 
+  if(depth > 5 & depth <= 10) {
+    nzones <- 2
+    z1 <- depth + 1
+    z2 <- 7
+    sed_amp_z1 <- airtemp_amp
     sed_amp_z2 <- airtemp_amp * exp(-z2/depth)
+    sed_doy_z1 <- airtemp_peakdoy
     # smooth out the air temp
     avg_airtemps$smoothed <- stats::filter(
       avg_airtemps$Temp,
@@ -36,8 +41,29 @@ get_sed_zone_data <- function(era5, depth){
       sides = 2
     )
     mean_temp <- mean(avg_airtemps$smoothed, na.rm = T)
-    sed_doy_z1 <- which(diff(avg_airtemps$smoothed > mean_temp) != 0)[-1]
     sed_doy_z2 <- (sed_doy_z1 + airtemp_peakdoy) / 1.8
-    return(cbind(sed_amp_z1, sed_amp_z2, sed_doy_z1, sed_doy_z2))
+    return(cbind(sed_amp_z1, sed_amp_z2, sed_doy_z1, sed_doy_z2, 
+                 nzones, z1, z2))
+  } else {
+    nzones <- 3
+    z1 <- depth + 1
+    z2 <- z1*2/3
+    z3 <- z1*1/3
+    sed_amp_z1 <- airtemp_amp
+    sed_amp_z2 <- airtemp_amp * exp(-z2/depth)
+    sed_amp_z3 <- airtemp_amp * exp(-z3/depth)
+    sed_doy_z1 <- airtemp_peakdoy
+    # smooth out the air temp
+    avg_airtemps$smoothed <- stats::filter(
+      avg_airtemps$Temp,
+      rep(1/14, 14),   # 2 week moving average
+      sides = 2
+    )
+    mean_temp <- mean(avg_airtemps$smoothed, na.rm = T)
+    sed_doy_z3 <- which(diff(avg_airtemps$smoothed > mean_temp) != 0)[-1]
+    sed_doy_z2 <- (sed_doy_z1 + airtemp_peakdoy) / 1.8
+    return(cbind(sed_amp_z1, sed_amp_z2, sed_amp_z3, 
+                 sed_doy_z1, sed_doy_z2, sed_doy_z3, 
+                 nzones, z1, z2, z3))
   }
 }
