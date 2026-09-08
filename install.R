5112a7583bd64c145148e76572b8396e8fdfb1cc
mollystroud <mollykstroud@gmail.com>
2026-09-08 17:33:22 -0400

    comment out

--- install.R
+++ install.R
@@ -4,10 +4,10 @@
 ################################################################################
 
 # This script will install all necessary packages to run FLARE
-install.packages(c('remotes', 'pacman'))
-remotes::install_github('rqthomas/GLM3r')
-remotes::install_github("FLARE-forecast/FLAREr")
-remotes::install_github('usgs-r/glmtools', force = T, upgrade = 'never')
-remotes::install_github("FLARE-forecast/ropenmeteo", force = T, upgrade = "never")
+# install.packages(c('remotes', 'pacman'))
+# remotes::install_github('rqthomas/GLM3r')
+# remotes::install_github("FLARE-forecast/FLAREr")
+# remotes::install_github('usgs-r/glmtools', force = T, upgrade = 'never')
+# remotes::install_github("FLARE-forecast/ropenmeteo", force = T, upgrade = "never")
 
 
